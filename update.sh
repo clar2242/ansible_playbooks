b44d4102f3e550ef74c9967c9ff90d9029737b32
Scott Clark <clar2242@me.com>
2025-04-25 08:42:35 +0100

    Latest Updates

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# sign in to onepassword (stops multiple prompts to auth)
 op signin
 op whoami
 
+# disable screensave
+defaults -currentHost write com.apple.screensaver idleTime 0
+
 ansible-playbook -e@credentials playbooks/update.yml
 ansible-playbook -e@credentials playbooks/docker.yml
+
+# enable screensaver
+defaults -currentHost write com.apple.screensaver idleTime 300
